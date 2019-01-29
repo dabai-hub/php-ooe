b6c69f67d6f8d8175c4dabaa487e61bba582d727
StudyForZX <479431650@qq.com>
2019-01-29 23:48:49 +0800

    phpunit

--- Tests/ArrayOoeTest.php
+++ Tests/ArrayOoeTest.php
@@ -30,6 +30,27 @@ class ChangeKeyCaseTest extends TestCase
     }
 
     /**
+     * 当返回的是数组的时候将属性值覆盖
+     *
+     * @param [type] $array
+     * @return void
+     */
+    public function generateNewArray($func, $origins, $expecteds, $params)
+    {
+        $this->instance->array($origins);
+
+        $this->assertInstanceOf(ArrayOoe::class, $this->instance);
+
+        $this->instance->{$func}(...$params);
+
+        $this->assertInstanceOf(ArrayOoe::class, $this->instance);
+
+        $actual = $this->instance->get();
+
+        $this->assertEquals($expecteds, $actual);
+    }
+
+    /**
      * @test
      */
     public function testChangeKeyCase()
@@ -37,13 +58,12 @@ class ChangeKeyCaseTest extends TestCase
         $origins = ['A' => 1, 'B' => 2, 'C' => 3, 'D' => 4];
         $expecteds = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4];
 
-        $ins = new ArrayOoe($origins);
-
-        $actual = $ins->changeKeyCase()->get();
-
-        $this->assertEquals(expecteds, $actual);
+        $this->generateNewArray(__FUNCTION__, $origins, $expecteds);
     }
 
+    /**
+     * @test
+     */
     public function testChunk()
     {
         $origins = [1, 2, 3, 4];
@@ -52,10 +72,23 @@ class ChangeKeyCaseTest extends TestCase
             [3, 4],
         ];
 
-        $ins = new ArrayOoe($origins);
-
-        $actual = $ins->chunk()->get();
-
-        $this->assertEquals(expecteds, $actual);
+        $this->generateNewArray(__FUNCTION__, $origins, $expecteds, [2]);
     }
+
+    /**
+     * @test
+     */
+    public function column()
+    {
+        $origins = [
+            ['a' => 11, 'b' => 12, 'c' => 13],
+            ['a' => 21, 'b' => 22, 'c' => 23],
+            ['a' => 31, 'b' => 32, 'c' => 33],
+        ];
+
+        $expecteds = [11, 21, 31];
+
+        $this->generateNewArray(__FUNCTION__, $origins, $expecteds, ['a']);
+    }
+
 }
